c1efd8030dcb20b2abf3def4cec5860b3c1a7eed
Bahar347 <bahar.be347@gmail.com>
2020-04-03 01:02:09 +0430

    movie ranking model

--- ranking/movies/models.py
+++ ranking/movies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from member.models import Member
 
 
 class Movie(models.Model):
@@ -8,3 +9,9 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class MovieRanking(models.Model):
+    member_id = models.ForeignKey(Member, on_delete=models.CASCADE)
+    movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    rank = models.IntegerField()
